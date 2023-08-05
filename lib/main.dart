b4c6fd053b1403bd539468f9b05739fb247b0356
jsmile <jsmile@naver.com>
2023-08-05 23:24:10 +0900

    error - to read a provider in a different route

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:bloc_named/show_me_counter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubits/counter/counter_cubit.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Named Route',
+      theme: ThemeData(
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          // useMaterial3: true,
+          primarySwatch: Colors.blue),
+      routes: {
+        '/': (context) =>
+            const MyHomePage(title: 'Flutter Named Route Home Page'),
+        '/counter': (context) => const ShowMeCounter(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text(
+                'Show me Counter',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/counter',
+                );
+              },
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Increase Counter',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              onPressed: () {
+                context.read<CounterCubit>().increment();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
